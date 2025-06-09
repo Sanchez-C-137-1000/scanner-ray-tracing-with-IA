53519d26f5523d78340a5bf55752341d9eace66e
Shekhina <shekhinagv@gmail.com>
2025-06-09 15:15:21 -0500

    raytracer.h

--- scr/render/raytracer.h
+++ scr/render/raytracer.h
@@ -1,5 +1,7 @@
 // Codigo realizado por: javier triana sanchez
 // Fecha de creacion: mayo 29 2025
+// Edicion: Junio 9 2025
+// Realizada por: Shekhina Velasquez 
 // Descripcion:
 //Implementar el algoritmo de trazado de rayos con iluminación básica.
 #pragma once
@@ -10,7 +12,7 @@
 
 class RayTracer {
 public:
-    void render(const std::vector<Triangle> & triangles, int width, int height);
+    void render(const std::vector<Triangle>& triangles, int width, int height, std::vector<Vec3>& image);
 private:
     Vec3 traceRay(const Ray& ray, const std::vector<Triangle >& triangles);
     bool intersect(const Ray& ray, const Triangle & tri, float& t);
